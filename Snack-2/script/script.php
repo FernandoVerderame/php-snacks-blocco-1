547525b79b1faf86d65b9e09bfd5d71685fbe1ed
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-19 16:24:35 +0100

    added validation form & message

--- Snack-2/script/script.php
+++ Snack-2/script/script.php
@@ -9,4 +9,22 @@ $user_email = $_GET['email'];
 // User age
 $user_age = $_GET['age'];
 
+if (strlen($username) > 3 && str_contains($user_email, '.') && str_contains($user_email, '@') && is_numeric($user_age)) {
+    $message = 'Accesso riuscito';
+} else {
+    $message = 'Accesso negato';
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 2</title>
+</head>
+<body>
+    <h2><?= $message ?></h2>
+</body>
+</html>
