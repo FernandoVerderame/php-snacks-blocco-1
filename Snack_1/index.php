cfc33797ace5986e289b8e23523143e85e1df70c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-19 15:23:44 +0100

    added matches array

---
+++ Snack_1/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+// Basket teams array
+$matches = [
+    [
+        'home_team' => 'Olimpia Milano',
+        'away_team' => 'Brindisi',
+        'home_score' => 58,
+        'away_score' => 40
+    ],
+    [
+        'home_team' => 'Napoli',
+        'away_team' => 'Cremona',
+        'home_score' => 73,
+        'away_score' => 56
+    ],
+    [
+        'home_team' => 'Virtus Bologna',
+        'away_team' => 'Scafati',
+        'home_score' => 61,
+        'away_score' => 84
+    ],
+    [
+        'home_team' => 'Brescia',
+        'away_team' => 'Varese',
+        'home_score' => 66,
+        'away_score' => 59
+    ]
+];
+
+var_dump($matches);
